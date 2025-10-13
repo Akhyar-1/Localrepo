97567528c96cf2d6d4aa9638a9d0437ee7a90816
akhyarrajpoot <akhyarrajput07@gmail.com>
2025-10-13 08:24:47 -0700

    my second commit'

--- chapter3/01 intra to string .py
+++ chapter3/01 intra to string .py
@@ -1,7 +1,10 @@
-name = "akhyar"
+# name = "akhyar"
 
-nameshort  = name[0:3] #a short from indax 0 all the way till 3 (excluding 3)
+# nameshort  = name[0:3] #a short from indax 0 all the way till 3 (excluding 3)
 
-print(nameshort)
-character = name[1]
-print(character)
+# print(nameshort)
+# character = name[1]
+# print(character)
+
+a = '21.gvhghgu'
+print(type(a))
